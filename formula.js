b13b9469fed73f870f28385c74122ebe99d9f229
Grace Ganzel <ganzelgr@umich.edu>
2018-11-14 17:46:52 -0500

    added the other formula, but didn't implement it or double check it

--- formula.js
+++ formula.js
@@ -3,26 +3,26 @@ var prompt = require('prompt');
 
 function getCVDRiskLipid(obj){
     var exponentSum  =
-        -0.1795 * obj['gender'] 
+        -0.1795 * obj['gender']
         + 3.78124 * (Math.log(obj['age']) - 3.853361)
-        + 0.007651 * (obj['sbp']-129.8673) 
-        + 0.625719 * obj['hyp'] 
+        + 0.007651 * (obj['sbp']-129.8673)
+        + 0.625719 * obj['hyp']
         - 0.00796 * obj['hyp'] * (obj['sbp'] - 129.8673)
-        + 0.11763 * (obj['chol'] - 5.562413) 
+        + 0.11763 * (obj['chol'] - 5.562413)
         - 0.8183 * (obj['hdl']-1.389071)
         + 0.37734 * obj['dm']
-        + 0.01639 * obj['ex'] 
-        + 0.29659 * obj['curr'] 
-        - 0.07043 * (obj['cal'] - 2001.83) 
+        + 0.01639 * obj['ex']
+        + 0.29659 * obj['curr']
+        - 0.07043 * (obj['cal'] - 2001.83)
         + 0.2104 * obj['dep']
-        + 0.41392 * obj['alc'] 
-        + 0.10963 * obj['t2'] 
-        + 0.16388 * obj['t3'] 
+        + 0.41392 * obj['alc']
+        + 0.10963 * obj['t2']
+        + 0.16388 * obj['t3']
         + 0.1828 * obj['t4']
-        + 0.22126 * obj['t5'] 
+        + 0.22126 * obj['t5']
         + 0.11177 * obj['bip']
-        + 0.21004 * obj['oth'] 
-        + 0.01526 * obj['reg'] 
+        + 0.21004 * obj['oth']
+        + 0.01526 * obj['reg']
         + 0.12121 * obj['atyp'];
 
     var S = 0.968011;
@@ -33,7 +33,37 @@ function getCVDRiskLipid(obj){
 }
 
 
+function getCVDRiskDesk(obj){
+    var exponentSum  =
+        -0.49376 * obj['gender']
+        + 3.50943 * (Math.log(obj['age']) - 3.853361)
+        + 0.00893 * (obj['sbp']-129.8673)
+        + 0.65817264 * obj['hyp']
+        - 0.00888 * obj['hyp'] * (obj['sbp'] - 129.8673)
+        + 0.000680 * (obj['wt'] - 76.20105)
+        - 0.0124 * (obj['ht'] - 167.9494)
+        + 0.44971 * obj['dm']
+        + 0.0738 * obj['ex']
+        + 0.38081 * obj['curr']
+        - 0.07524 * (obj['cal'] - 2001.83)
+        + 0.21846 * obj['dep']
+        + 0.30721 * obj['alc']
+        + 0.10919 * obj['t2']
+        + 0.18412 * obj['t3']
+        + 0.20238 * obj['t4']
+        + 0.24762 * obj['t5']
+        + 0.0978 * obj['bip']
+        + 0.19063 * obj['oth']
+        - 0.01138 * obj['reg']
+        + 0.17662 * obj['atyp']
+        + 0.1205 * obj['typ'];
 
+    var S = 0.951285;
+
+    var riskscore = 100 * (1 - Math.pow(S, Math.exp(exponentSum)));
+
+    return riskscore;
+}
 
 
 
